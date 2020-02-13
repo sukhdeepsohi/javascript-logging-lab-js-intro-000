43e8529c251332ece99535d2bd6d2b1632a35d54
debonair-product-3719 <sukhdeepsohi@gmail.com>
2020-02-13 22:45:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log("I would be a logger.") // get it?
 console.error("HALP")
 console.log("fuck her")// guilda
-console/log("guilda run")// get fucked
+console/warn("guilda run")// get fucked
